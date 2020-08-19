ba93ee725f6681e6b8af40ef32371d85555f9ce6
taehyundev <K.anon.11107@gmail.com>
2020-08-19 23:51:58 +0900

    많은 양의 데이터 입력시 POST 요청

--- 실습파일/JSP/JSPPrj/src/com/taehyundev/web/NoticeReg.java
+++ 실습파일/JSP/JSPPrj/src/com/taehyundev/web/NoticeReg.java
@@ -9,22 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello")
-public class Nana extends HttpServlet{
+@WebServlet("/hi")
+public class NoticeReg extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		res.setCharacterEncoding("UTF-8");
 		res.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = res.getWriter();
-		//requset 입력도구 , response 출력도구
-		String temp = req.getParameter("n");
-		//기본값
-		int cnt = 100;
-		if(temp != null && !temp.equals(""))
-			cnt = Integer.parseInt(temp);
-		
-		for(int i=0; i<cnt;i++)
-			out.println((i+1)+" : Hello ~~~<br>");
+		String title=req.getParameter("title");
+		String contents = req.getParameter("contents");
+		out.println(title+"<br>"+contents);
 	}
 }
